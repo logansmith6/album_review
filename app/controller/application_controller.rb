2c062604ff391560ffadbf20421be855fa146335
Logan Smith <logsmith06@gmail.com>
2020-02-14 21:10:49 +0000

    added more helper methods

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -21,6 +21,14 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!session[:email]
     end
+
+    def login(email)
+      session[:email] = email
+    end
+
+    def logout!
+      session.clear
+    end
   end
 
 
