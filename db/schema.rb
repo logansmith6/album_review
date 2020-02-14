3fbcf4704ae387e952e7ebb20fbd775516fa92b4
Logan Smith <logsmith06@gmail.com>
2020-02-14 23:57:35 +0000

    added reviews class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_184049) do
+ActiveRecord::Schema.define(version: 2020_02_14_235542) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.string "genre"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
