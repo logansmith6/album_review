db45879076296d865691b25cfecb5c5535176382
Logan Smith <logsmith06@gmail.com>
2020-02-14 20:27:35 +0000

    added database.yml file to get successful migations

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
 require_relative './config/environment'
 
+require 'sinatra/activerecord'
 require 'sinatra/activerecord/rake'
 
 desc 'A console'
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
 require 'bundler'
 
 Bundler.require
-
+configure :development do
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/development.sqlite"
+  :database => "db/development.sqlite3"
 )
+end
 require_all 'app'
