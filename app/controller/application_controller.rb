b37f6a8aace4be3a5102e62da24b35b3b43d9440
Logan Smith <logsmith06@gmail.com>
2020-02-14 21:37:03 +0000

    small changes to applicationcontroller

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -1,34 +1,73 @@
+require "./config/environment"
+require "./app/models/user"
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, "app/views"
     enable :sessions
-    set :session_secret, "small_mountain"
+    set :session_secret, "password_security"
   end
 
-  get '/' do
-    redirect '/login'
+  get "/" do
+    erb :index
   end
 
-  get '/failure' do
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    #your code here
+    @user = User.new(:username => params[:username], :password => params[:password])
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+    else
+      @user.save
+      redirect "/login"
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    ##your code here
+    user = User.find_by(:username => params[:username])
+
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+    else
+      if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/account'
+    end
+  end
+end
+
+  get "/failure" do
     erb :failure
   end
 
-
-  helpers do
-
-    def logged_in?
-      !!session[:username]
-    end
-
-
-
-    def logout!
-      session.clear
-    end
+  get "/logout" do
+    session.clear
+    redirect "/"
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
