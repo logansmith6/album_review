820bda66c9ad6da86e8996387e4d16e0361010a4
Logan Smith <logsmith06@gmail.com>
2020-02-15 01:03:17 +0000

    added post /reviews route

--- app/controller/reviews_controller.rb
+++ app/controller/reviews_controller.rb
@@ -10,6 +10,21 @@ class ReviewsController < ApplicationController
     end
   end
 
+  post '/reviews' do
+    @review = Review.create(:album_name => params[:album_name], :genre => params[:genre],
+                            :content => params[:content], :rating => params[:rating],
+                            :user_id => params[:user_id])
+
+        if params[:album_name].empty? || params[:genre].empty? || params[:content].empty?
+                || params[:user_id].empty? || params[:rating].empty?
+                redirect '/reviews/new'
+              else
+                @review.save
+                redirect to "/reviews/#{@review.id}"
+              end
+
+  end
+
   get '/reviews/new' do
     if !logged_in?
       redirect '/login'
