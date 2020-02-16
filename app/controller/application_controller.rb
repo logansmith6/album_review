e250a006d562c099fb9a61168bbab8875a998d85
Logan Smith <logsmith06@gmail.com>
2020-02-16 02:25:27 +0000

    commented application_controller.rb

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -7,22 +7,23 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "small_mountain"
   end
 
+# sets "homepage" to be the login page
   get '/' do
     redirect '/login'
   end
-
+# if sent here, failure message will be displayed from failure.erb
   get '/failure' do
     erb :failure
   end
 
-
+#helper functions
   helpers do
 
-    def logged_in?
+    def logged_in? #if logged in, returns true
       !!user_online
     end
 
-    def login(username, password)
+    def login(username, password) #function to login user
       user = User.find_by(:username => username)
       if user && user.authenticate(password)
         session[:username] = user.username
@@ -31,17 +32,13 @@ class ApplicationController < Sinatra::Base
       end
     end
 
-    def logout!
+    def logout! #logs out / clears the session
       session.clear
     end
 
-    def user_online
+    def user_online #returns the current user of the session
       @user_online ||= User.find_by(:username => session[:username]) if session[:username]
     end
 
-
   end
-
-
-
 end
